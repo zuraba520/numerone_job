ad7bd28a5f0a9104bda62958f0d4359d8a7a60a5
gizu <zuraba000@gmail.com>
2023-08-18 03:03:26 +0400

    add new files

---
+++ lib/expenses.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:numerone_job/expenses_list.dart';
+import 'package:numerone_job/models/expense.dart';
+
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'flutter course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:numerone_job/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
